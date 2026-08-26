dd386c7000b2d20b7adabd5b8178180c7f6c9831
ShreyasVH <44293779+ShreyasVH@users.noreply.github.com>
2026-08-26 23:14:32 +0530

    update the ci steps to push both arm and amd images
    * building both platforms
    
    * defining image name
    
    * using environment
    
    * passing VERSION correctly
    
    * corrected image name
    
    * reverted test changes

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
+export VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
+
 docker compose -f docker-compose.yml build --no-cache
 
 docker compose -f docker-compose-dev.yml build --no-cache
